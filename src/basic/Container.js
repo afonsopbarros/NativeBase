95371c3e0d38698ed42695f5a2b18cab3f44d27d
afonsopbarros <afonso.soveral.barros@gmail.com>
2021-03-25 12:54:44 +0000

    use context of height

--- src/basic/Container.js
+++ src/basic/Container.js
@@ -1,50 +1,83 @@
 import React, { Component } from 'react';
+import { KeyboardAvoidingView, View, Platform } from 'react-native';
+import {
+  SafeAreaView,
+  SafeAreaFrameContext
+} from 'react-native-safe-area-context';
 import PropTypes from 'prop-types';
 import { connectStyle } from 'native-base-shoutem-theme';
 
 import { ViewPropTypes } from '../utils';
 import mapPropsToStyleNames from '../utils/mapPropsToStyleNames';
-import { KeyboardAvoidingView } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { Platform } from 'react-native';
-import { View } from 'react-native';
 
 class Container extends Component {
   render() {
     if (this.props.noSafeArea) {
       return Platform.OS === 'ios' ? (
-        <KeyboardAvoidingView
-          behavior={'padding'}
-          style={{ flex: 1 }}
-          ref={c => (this._root = c)}
-          {...this.props}
-        >
-          {this.props.children}
-        </KeyboardAvoidingView>
+        <SafeAreaFrameContext.Consumer>
+          {frame => (
+            <KeyboardAvoidingView
+              behavior={'padding'}
+              style={{
+                flex: 1,
+                height: Platform.OS === 'ios' ? frame.height : frame.height - 20
+              }}
+              ref={c => (this._root = c)}
+              {...this.props}
+            >
+              {this.props.children}
+            </KeyboardAvoidingView>
+          )}
+        </SafeAreaFrameContext.Consumer>
       ) : (
-        <View style={{ flex: 1 }} ref={c => (this._root = c)} {...this.props}>
-          {this.props.children}
-        </View>
+        <SafeAreaFrameContext.Consumer>
+          {frame => (
+            <View
+              style={{
+                flex: 1,
+                height: Platform.OS === 'ios' ? frame.height : frame.height - 20
+              }}
+              ref={c => (this._root = c)}
+              {...this.props}
+            >
+              {this.props.children}
+            </View>
+          )}
+        </SafeAreaFrameContext.Consumer>
       );
     }
     return Platform.OS === 'ios' ? (
-      <SafeAreaView
-        style={{ flex: 1 }}
-        ref={c => (this._root = c)}
-        {...this.props}
-      >
-        <KeyboardAvoidingView behavior={'padding'} style={{ flex: 1 }}>
-          {this.props.children}
-        </KeyboardAvoidingView>
-      </SafeAreaView>
+      <SafeAreaFrameContext.Consumer>
+        {frame => (
+          <SafeAreaView
+            style={{
+              flex: 1,
+              height: Platform.OS === 'ios' ? frame.height : frame.height - 20
+            }}
+            ref={c => (this._root = c)}
+            {...this.props}
+          >
+            <KeyboardAvoidingView behavior={'padding'} style={{ flex: 1 }}>
+              {this.props.children}
+            </KeyboardAvoidingView>
+          </SafeAreaView>
+        )}
+      </SafeAreaFrameContext.Consumer>
     ) : (
-      <SafeAreaView
-        style={{ flex: 1 }}
-        ref={c => (this._root = c)}
-        {...this.props}
-      >
-        {this.props.children}
-      </SafeAreaView>
+      <SafeAreaFrameContext.Consumer>
+        {frame => (
+          <SafeAreaView
+            style={{
+              flex: 1,
+              height: Platform.OS === 'ios' ? frame.height : frame.height - 20
+            }}
+            ref={c => (this._root = c)}
+            {...this.props}
+          >
+            {this.props.children}
+          </SafeAreaView>
+        )}
+      </SafeAreaFrameContext.Consumer>
     );
   }
 }
